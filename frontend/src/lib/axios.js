0ea70538198c5f7bd47b9028dfc1a3ee9fb50500
Haluk Canpolat <halukcnplt@gmail.com>
2026-02-06 00:29:15 +0300

    feat: Add Dockerization and docker-compose for all frontend and backend services.

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api/weather",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api/weather",
 });
